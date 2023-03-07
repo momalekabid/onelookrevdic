53fd7ffa6cc683c87dd0dcf40b0fa5fbae28ac34
Malek <moabid75@gmail.com>
2023-03-07 05:49:53 +0000

    smal clean

--- main.py
+++ main.py
@@ -7,36 +7,34 @@ from data_process import *
 file_types = ["train", "dev", "seen", "unseen", "desc"]
 
 
-def process_data():
+def process_data(ftype):
     print("Processing data:")
-    ftype = file_types[0]
     file = f"./data/wwdata/{ftype}.json"
     out_file  = f"./data/wwdata/{ftype}_stripped.json"
     process_tsinghua_data(file, out_file)
-
     print("Linguistic annotations successfully stripped")
-    #file = f"./data/wwdata/{ftype}2.json"
-    #out_file  = f"./data/wwdata/{ftype}3.json"
-    #add_fasttext_embedding(file, out_file)
-    #print("Fasttext embeddings completed")
-
 
     file = f"./data/wwdata/{ftype}_stripped.json"
-    out_file  = f"./data/wwdata/{ftype}_w2v.json"
-    add_word2vec_embedding(file, out_file, ftype)
-    print("Word2vec embeddings completed.")
+    out_file  = f"./data/wwdata/{ftype}_ft.json"
+    add_fasttext_embedding(file, out_file)
+    print("Fasttext embeddings completed")
 
-    #file = f"./data/wwdata/{ftype}3.json"
-    #out_file  = f"./data/wwdata/{ftype}4.json"
+    #file = f"./data/wwdata/{ftype}_stripped.json"
+    #out_file  = f"./data/wwdata/{ftype}_w2v.json"
+    #add_word2vec_embedding(file, out_file, ftype)
+    #print("Word2vec embeddings completed.")
+
+    #file = f"./data/wwdata/{ftype}_stripped.json"
+    #out_file  = f"./data/wwdata/{ftype}_bert.json"
     #add_bert_embedding_from_def(file, out_file, ftype)
     #print("BERT embeddings completed.")
 
-    file = f"./data/wwdata/{ftype}_w2v.json"
-    out_file  = f"./data/wwdata/{ftype}_w2v.json"
+    file = f"./data/wwdata/{ftype}_ft.json"
+    out_file  = f"./data/wwdata/{ftype}_ft.json"
     filter_hill_data(file, out_file)
     print("Duplicates cleaned successfully")
 
-    file = f"./data/wwdata/{ftype}_w2v.json"
+    file = f"./data/wwdata/{ftype}_ft.json"
     # out_file  = f"./data/wwdata/{ftype}_label_list.json"
     try:
         check_data(file)
@@ -44,4 +42,5 @@ def process_data():
         print("Some errors^")
     print("Complete.")
 
-process_data()
+#process_data(file_types[0]) #train
+process_data(file_types[1]) #dev
